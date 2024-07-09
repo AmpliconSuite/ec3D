Fix bugs in RMSD and PCC calculation

src/spatial_structure.py
@@ -15,7 +15,7 @@ from sklearn.metrics import euclidean_distances
 from autograd import grad
 
 
-from util import calculate_average_distance, remove_nan_col, getTransformation
+from util import getTransformation
 
 
 def compute_wish_distances(C_, alpha = -3.0, beta = 1.0):
@@ -244,6 +244,7 @@ def exponent_obj(x, X, C_nodup, S = None, C_dup = None, beta = 1.0):
 			raise ValueError("Function evaluation returns nan.")
 		return ll_alpha
 
+
 def exponent_obj_alpha_auto(x, X, C_nodup, S = None, C_dup = None, beta = 1.0, reg = True, gamma = 0.01):
 	"""
 	Objective function for optimizing alpha
@@ -317,7 +318,8 @@ def exponent_obj_alpha_auto(x, X, C_nodup, S = None, C_dup = None, beta = 1.0, r
 		if auto_np.isnan(ll_alpha):
 			raise ValueError("Function evaluation returns nan.")
 		return ll_alpha
-	
+
+
 def exponent_obj_beta_auto(x, X, C_nodup, S = None, C_dup = None, alpha = -3, reg = True, gamma = 0.01):
 	"""
 	Objective function for optimizing alpha
@@ -391,6 +393,7 @@ def exponent_obj_beta_auto(x, X, C_nodup, S = None, C_dup = None, alpha = -3, re
 		if auto_np.isnan(ll_beta):
 			raise ValueError("Function evaluation returns nan.")
 		return ll_beta
+
 
 def exponent_gradient(x, X, C_nodup, S = None, C_dup = None, beta = 1.0):
 	"""
@@ -646,6 +649,7 @@ def poisson_obj_reg_auto(x, N, C_nodup, S = None, C_dup = None, idx_map = None, 
 			raise ValueError("Function evaluation returns nan.")
 		return obj
 
+
 def convergence_criteria(f_k_list, f_k_len = 10, factr = 1e9):
 	"""
 	Convergence criteria for joint inference of alpha, beta and structure.
@@ -660,7 +664,7 @@ def convergence_criteria(f_k_list, f_k_len = 10, factr = 1e9):
 		return (dif <= factr * np.finfo(float).eps)
 
 
-def max_poisson_likelihood(C, N, ini_x, ini_C1, idx_nodup, idx_dup, dup_times, idx_map, round = 5000, alpha = -3.0, beta = 1.0, maxiter = 10000, start_time_ = None, gt_structure = None):
+def max_poisson_likelihood(C, N, ini_x, ini_C1, idx_nodup, idx_dup, dup_times, idx_map, round = 5000, alpha = -3.0, beta = 1.0, reg_weight = 0.05, maxiter = 10000, start_time_ = None, gt_structure = None):
 	"""
 	Poisson model caller
 	"""
@@ -697,13 +701,14 @@ def max_poisson_likelihood(C, N, ini_x, ini_C1, idx_nodup, idx_dup, dup_times, i
 	alpha_gradient = grad(exponent_obj_alpha_auto)
 	beta_gradient = grad(exponent_obj_beta_auto)
 	poisson_gradient = grad(poisson_obj_reg_auto)
-	gamma = 0.0
+	gamma = reg_weight
 	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time_) + "Regularizer weight %f * N_e." %(gamma))
 	for it in range(round):
 		print(f'Poisson iteration {it+1}')
 		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time_) + "Begin iteration %d." %(it + 1))
 		
 		# estimate alpha and beta by autograd
+		"""
 		results1 = optimize.fmin_l_bfgs_b(exponent_obj_alpha_auto, np.array([alpha]), fprime = alpha_gradient,
 						args = (X_, C_nodup, S, C_dup, beta, True, gamma), bounds = alpha_bounds_, maxiter = 1000)
 		alpha = results1[0][0]
@@ -711,7 +716,8 @@ def max_poisson_likelihood(C, N, ini_x, ini_C1, idx_nodup, idx_dup, dup_times, i
 						args = (X_, C_nodup, S, C_dup, alpha, True, gamma), bounds = beta_bounds_, maxiter = 1000)
 		beta = results2[0][0]
 		f_alpha = results2[1]
-		# alpha, beta, f_alpha = estimate_alpha_beta(X_, C_nodup, S, C_dup, alpha, beta)
+		"""
+		alpha, beta, f_alpha = estimate_alpha_beta(X_, C_nodup, S, C_dup, alpha, beta)
 
 		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time_) + "\tEstimated alpha = %f; beta = %f." %(alpha, beta))
 		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time_) + 
@@ -722,12 +728,8 @@ def max_poisson_likelihood(C, N, ini_x, ini_C1, idx_nodup, idx_dup, dup_times, i
 		X_ = results[0].reshape(-1, 3)
 		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time_) + "\tLog likelihood at iteration %d = %f." %(it + 1, results[1]))
 		if gt_structure != None:
-			scale_factor = calculate_average_distance(X_original)
-			fr_pos_array = X_original / scale_factor
-			mds_pos_array = X_[idx_map] / scale_factor
-			fr_pos_array = remove_nan_col(fr_pos_array)
-			mds_pos_array = remove_nan_col(mds_pos_array)
-			rmsd, X1, _, pcc = getTransformation(mds_pos_array,fr_pos_array)
+			structure1, structure2 = X_[idx_map], X_original
+			rmsd, X1, X2, pcc = getTransformation(structure1, structure2) # structure1 is transformed
 			# np.savetxt(f'simple_structure_{it}.txt', X1)
 			logging.info("#TIME " + '%.4f\t' %(time.time() - start_time_) + "\tRMSD = %f\tPCC = %f" %(rmsd, pcc))
 		if convergence_criteria(obj_list):
@@ -738,23 +740,35 @@ def max_poisson_likelihood(C, N, ini_x, ini_C1, idx_nodup, idx_dup, dup_times, i
 		else:
 			obj_list.pop(0)
 			obj_list.append(results[1])
-	return X_, C1_
+	return X_, results[1], alpha, beta
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "Compute the 3D coordinates from Hi-C.")
-	parser.add_argument("--matrix", help = "Input Hi-C matrix, in *.txt or *.npy format.", required = True)
+	parser.add_argument("--matrix", help = "Input collapsed Hi-C matrix, in *.txt or *.npy format.", required = True)
 	parser.add_argument("--annotation", help = "Annotation of bins in the input matrix.", required = True)
 	parser.add_argument("--output_prefix", help = "Prefix of output files.", required = True)
-	parser.add_argument("--log_fn", help = "Name of log file.", default = "spatial_structure.log")
-	parser.add_argument("--structure", help = "Input the original structure, in *.txt or *.npy format, for calculating RMSD and PCC.", default=None)
+	parser.add_argument("--log_fn", help = "Name of log file.")
+	parser.add_argument("--reg", help = "Regularizer weight.", type = float, default = 0.05)
+	parser.add_argument("--init_alpha", help = "An initial guess of alpha, for initialization and MDS.", type = float, default = -3.0)
+	parser.add_argument("--num_repeats", help = "Number of repetitions with random initial structures.", type = int, default = 5)
+	parser.add_argument("--max_rounds", help = "Maximum number of rounds for Poisson model.", type = int, default = 1000)
+	parser.add_argument("--max_iter_likelihood", help = "Maximum number of iterations for estimating the structure with L-BFGS.", type = int, default = 10000)
+	parser.add_argument("--max_iter_exponent", help = "Maximum number of iterations for estimating alpha/beta with L-BFGS.", type = int, default = 5000)
+	parser.add_argument("--structure", help = "Input the true structure, in *.txt or *.npy format, for calculating RMSD and PCC.")
+	
 	start_time = time.time()
 	args = parser.parse_args()
 
 	"""
 	Set up logging
 	"""
-	logging.basicConfig(filename = args.log_fn, filemode = 'w', level = logging.DEBUG, 
+	log_fn = ""
+	if not args.log_fn:
+		log_fn = args.output_prefix + "_optimization.log"
+	else:
+		log_fn = args.log_fn
+	logging.basicConfig(filename = log_fn, filemode = 'w', level = logging.DEBUG, 
 						format = '[%(name)s:%(levelname)s]\t%(message)s')
 	logging.info("Python version " + sys.version + "\n")
 	commandstring = 'Command line: '
@@ -764,6 +778,14 @@ if __name__ == "__main__":
 		else:
 			commandstring += "{} ".format(arg)
 	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + commandstring)
+
+	"""
+	Parameter check
+	"""
+	if args.num_repeats < 1 or args.num_repeats > 100:
+		raise OSError("Number of repetitions must stay in range [1, 100].")
+	if args.init_alpha >= 0:
+		raise OSError("Initial value of alpha must be less than 0.")
 
 	"""
 	Load ecDNA matrix
@@ -778,7 +800,7 @@ if __name__ == "__main__":
 	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Loaded normalized collapsed ecDNA matrix.")
 	
 	"""
-	Step 1: Construct Hi-C matrix with duplication
+	Construct Hi-C matrix with duplication
 	"""
 	N = -1 # Num bins in donor Hi-C
 	bins = []
@@ -802,7 +824,7 @@ if __name__ == "__main__":
 	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Collapsed ecDNA matrix size: %d." %len(row_labels))				
 
 	"""
-	Step2: Run MDS with alpha = -3 and beta = 1 to compute an initial X  
+	Run MDS with alpha = init_alpha to compute an initial X  
 	"""
 	bins = sorted(bins, key = lambda bin: row_labels[bin][0])
 	idx_nodup = [bi for bi in range(len(bins)) if len(row_labels[bins[bi]]) == 1]
@@ -812,7 +834,7 @@ if __name__ == "__main__":
 	C_nodup = C[np.ix_(idx_nodup, idx_nodup)]
 
 	"""
-	Step2-a: Initialize matrix with duplications
+	Initialize matrix with duplications
 	"""
 	ini_c = None
 	if N_nodup < N:
@@ -841,12 +863,12 @@ if __name__ == "__main__":
 				total_int_ij = C[bi_nodup][bi_dup]
 				di = row_labels[bins[bi_nodup]][0]
 				d_ij = np.array([min(abs(di - dj), abs(min(di, dj) - max(di, dj) + N)) for dj in row_labels[bins[bi_dup]]], dtype = float)
-				sum_d = sum(d_ij ** (-3.0))
+				sum_d = sum(d_ij ** (args.init_alpha))
 				for j_ in range(len(row_labels[bins[bi_dup]])):
 					try:
-						ini_c[ci][cj] = max(min(total_int_ij * 0.9, C_avg[int(d_ij[j_])]), total_int_ij / sum_d * (d_ij[j_] ** (-3.0)))
+						ini_c[ci][cj] = max(min(total_int_ij * 0.9, C_avg[int(d_ij[j_])]), total_int_ij / sum_d * (d_ij[j_] ** (args.init_alpha)))
 					except:
-						ini_c[ci][cj] = max(min(total_int_ij * 0.9, min_c_avg), total_int_ij / sum_d * (d_ij[j_] ** (-3.0)))
+						ini_c[ci][cj] = max(min(total_int_ij * 0.9, min_c_avg), total_int_ij / sum_d * (d_ij[j_] ** (args.init_alpha)))
 					cj += 1 
 			ci += 1
 		for bi1 in idx_dup:
@@ -861,23 +883,23 @@ if __name__ == "__main__":
 						di = row_labels[bins[bi1]][i_]
 						dj = row_labels[bins[bi2]][j_]
 						d_ij[i_][j_] = max(d_ij[i_][j_], min(abs(di - dj), abs(min(di, dj) - max(di, dj) + N)))
-				sum_d = (d_ij ** (-3.0)).sum()
+				sum_d = (d_ij ** (args.init_alpha)).sum()
 				for i_ in range(ni1):
 					for j_ in range(ni2):
 						if i_ != j_:
 							try:
-								ini_c[ci + i_][cj + j_] = max(min(total_int_ij * 0.9, C_avg[int(d_ij[i_][j_])]), total_int_ij / sum_d * (d_ij[i_][j_] ** (-3.0)))
+								ini_c[ci + i_][cj + j_] = max(min(total_int_ij * 0.9, C_avg[int(d_ij[i_][j_])]), total_int_ij / sum_d * (d_ij[i_][j_] ** (args.init_alpha)))
 							except:
-								ini_c[ci + i_][cj + j_] = max(min(total_int_ij * 0.9, min_c_avg), total_int_ij / sum_d * (d_ij[i_][j_] ** (-3.0)))
+								ini_c[ci + i_][cj + j_] = max(min(total_int_ij * 0.9, min_c_avg), total_int_ij / sum_d * (d_ij[i_][j_] ** (args.init_alpha)))
 						else:
-							ini_c[ci + i_][cj + j_] = total_int_ij / sum_d * (d_ij[i_][j_] ** (-3.0))
+							ini_c[ci + i_][cj + j_] = total_int_ij / sum_d * (d_ij[i_][j_] ** (args.init_alpha))
 				cj += ni2 
 			ci += ni1
 		#ini_c[ini_c < 1.0] = 1.0
 		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Initialized expanded ecDNA matrix for MDS.")
 	
 	"""
-	Step2-b: Run MDS
+	Map bin indices for optimization: place the bins without duplication in front
 	"""
 	i_nodup, i_dup = 0, 0
 	idx_map = dict()
@@ -890,79 +912,72 @@ if __name__ == "__main__":
 				idx_map[row_labels[bins[bi]][i_]] = N_nodup + i_dup
 				i_dup += 1
 	idx_map = np.array([idx_map[i] for i in range(len(idx_map))])
+	
+	repeat_ = 1
+	PM_X_min = np.zeros(N * 3)
+	PM_obj_min = np.inf
+	best_alpha = -3.0
+	best_beta = 1.0
+	for repeat in range(1, args.num_repeats + 1):
+		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Repeat %d:" %repeat)
 
-	a = -3
-	MDS_X1, MDS_X2 = mds(C, N, idx_nodup, idx_dup, dup_times, ini_c, alpha = a)
-	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "MDS optimization completed.")
-	# MDS_X1 = np.loadtxt(args.structure)
-	if args.structure == None:
-		np.savetxt(f'{args.output_prefix}_mds_3d.txt', MDS_X1[idx_map])
-	else:
-		original, reconstructed = np.loadtxt(args.structure), MDS_X1[idx_map]
-		scale_factor = calculate_average_distance(original)
-		fr_pos_array = original / scale_factor
-		mds_pos_array = reconstructed / scale_factor
-		fr_pos_array = remove_nan_col(fr_pos_array)
-		mds_pos_array = remove_nan_col(mds_pos_array)
-		rmsd, X1, X2, pcc = getTransformation(mds_pos_array,fr_pos_array)
-		np.savetxt(f'{args.output_prefix}_mds_3d.txt', X1)
-	"""
-	Step3: Run Poisson model with initial X and matrix returned from MDS
-	"""
-	num_rounds = 5000
-	PM_X1, PM_X2 = max_poisson_likelihood(C, N, MDS_X1, MDS_X2, idx_nodup, idx_dup, dup_times, idx_map, num_rounds, start_time_ = start_time, gt_structure = args.structure, alpha = a)
-	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Poisson model optimization completed.")
-	if N_nodup < N:
-		i_nodup, i_dup = 0, 0
-		idx_map = dict()
-		for bi in range(len(bins)):
-			if bi in idx_nodup:
-				idx_map[row_labels[bins[bi]][0]] = i_nodup
-				i_nodup += 1
+		"""
+		Run MDS
+		"""
+		MDS_X1, MDS_X2 = mds(C, N, idx_nodup, idx_dup, dup_times, ini_c, alpha = args.init_alpha)
+		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "MDS optimization completed.")
+		""" Disabled saving of MDS output
+		if args.structure == None:
+			np.savetxt(f'{args.output_prefix}_mds_{repeat}_3d.txt', MDS_X1[idx_map])
+		else:
+			original, reconstructed = np.loadtxt(args.structure), MDS_X1[idx_map]
+			scale_factor = calculate_average_distance(original)
+			fr_pos_array = original / scale_factor
+			mds_pos_array = reconstructed / scale_factor
+			fr_pos_array = remove_nan_col(fr_pos_array)
+			mds_pos_array = remove_nan_col(mds_pos_array)
+			rmsd, X1, X2, pcc = getTransformation(mds_pos_array,fr_pos_array)
+			np.savetxt(f'{args.output_prefix}_mds_{repeat}_3d.txt', X1)
+		"""
+		"""
+		Run Poisson model with initial X and matrix returned from MDS
+		"""
+		PM_X, PM_obj, alpha, beta = max_poisson_likelihood(C, N, MDS_X1, MDS_X2, idx_nodup, idx_dup, dup_times, idx_map, args.max_rounds, start_time_ = start_time, gt_structure = args.structure, alpha = args.init_alpha)
+		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Poisson model optimization completed.")
+		if PM_obj < PM_obj_min:
+			if np.isinf(PM_obj_min):
+				logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Reset the current best repetition to %d." %(repeat))
 			else:
-				for i_ in range(len(row_labels[bins[bi]])):
-					idx_map[row_labels[bins[bi]][i_]] = N_nodup + i_dup
-					i_dup += 1
-		idx_map = np.array([idx_map[i] for i in range(len(idx_map))])
-		D = np.block([[C_nodup, PM_X2[: N_nodup, :]], [PM_X2.T]])
+				logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Repetition %d showed a better objective than current best repetition %d." %(repeat, repeat_)) 
+			PM_obj_min = PM_obj
+			PM_X_min = PM_X
+			repeat_ = repeat
+			best_alpha = alpha
+			best_beta = beta
+	
+	"""
+	Write output to file
+	"""
+	if N_nodup < N:
 		"""
 		Reorder the result matrix
 		"""
-		D = D[np.ix_(idx_map, idx_map)]
-		PM_X1 = PM_X1[idx_map]
-		# output_matrix_fn = args.output_prefix + "_expanded_matrix.txt"
-		# logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Save resolved ecDNA Hi-C matrix into %s." %output_matrix_fn)
-		# np.savetxt(output_matrix_fn, D)
-		output_coordinates_fn = args.output_prefix + "_coordinates.txt"
-		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Save resolved ecDNA 3D structure into %s." %output_coordinates_fn)
-		np.savetxt(output_coordinates_fn, PM_X1)
-		if args.structure != None:
-			original, reconstructed = np.loadtxt(args.structure), PM_X1
-			scale_factor = calculate_average_distance(original)
-			fr_pos_array = original / scale_factor
-			mds_pos_array = reconstructed / scale_factor
-			fr_pos_array = remove_nan_col(fr_pos_array)
-			mds_pos_array = remove_nan_col(mds_pos_array)
-			rmsd, X1, X2, pcc = getTransformation(mds_pos_array,fr_pos_array)
-			output_coordinates_fn = args.output_prefix + "_aligned_coordinates.txt"
-			np.savetxt(output_coordinates_fn, X1)
-	else:
-		# output_matrix_fn = args.output_prefix + "_expanded_matrix.txt"
-		# logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Save ecDNA Hi-C matrix into %s." %output_matrix_fn)
-		# np.savetxt(output_matrix_fn, C)
-		output_coordinates_fn = args.output_prefix + "_coordinates.txt"
-		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Save resolved ecDNA 3D structure into %s." %output_coordinates_fn)
-		np.savetxt(output_coordinates_fn, PM_X1)
-		if args.structure != None:
-			original, reconstructed = np.loadtxt(args.structure), PM_X1
-			scale_factor = calculate_average_distance(original)
-			fr_pos_array = original / scale_factor
-			mds_pos_array = reconstructed / scale_factor
-			fr_pos_array = remove_nan_col(fr_pos_array)
-			mds_pos_array = remove_nan_col(mds_pos_array)
-			rmsd, X1, X2, pcc = getTransformation(mds_pos_array,fr_pos_array)
-			output_coordinates_fn = args.output_prefix + "_aligned_coordinates.txt"
-			np.savetxt(output_coordinates_fn, X1)
+		PM_X_min = PM_X_min[idx_map]
+	output_coordinates_fn = args.output_prefix + "_coordinates.txt"
+	np.savetxt(output_coordinates_fn, PM_X_min)
+	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Saved the resolved 3D structure to %s." %output_coordinates_fn)
+	output_params_fn = args.output_prefix + "_hyperparameters.txt"
+	fp_w = open(output_params_fn, 'w')
+	fp_w.write("alpha\t%f\n" %best_alpha)
+	fp_w.write("beta\t%f\n" %best_beta)
+	fp_w.close()
+	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Saved the hyperparameters to %s." %output_params_fn)
+	if args.structure != None:
+		structure1, structure2 = PM_X_min, np.loadtxt(args.structure)
+		rmsd, X1, X2, pcc = getTransformation(structure1, structure2) # structure1 is transformed
+		output_coordinates_fn = args.output_prefix + "_aligned_coordinates.txt"
+		np.savetxt(output_coordinates_fn, X1)
+		logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Saved the aligned 3D structure to %s." %output_coordinates_fn)
 	logging.info("#TIME " + '%.4f\t' %(time.time() - start_time) + "Total runtime.")
 
 

src/util.py
@@ -28,7 +28,7 @@ def read_ecDNA_cycle(fn, res):
 	return intrvls
 
 
-def reorder_bins(matrix, intrvls):
+def reorder_bins(matrix, intrvls, res):
 	"""
 	Correct the order of bins for segment with orientation '-'
 	"""
@@ -40,19 +40,6 @@ def reorder_bins(matrix, intrvls):
 			matrix[start: start + intrvl_size, :] = matrix[start: start + intrvl_size, :][::-1, :]
 		start += intrvl_size
 	return matrix
-
-def calculate_average_distance(array):
-	diff_array = array[:, None, :] - array[None, :, :]
-	distance_array = np.sqrt((diff_array**2).sum(-1))
-	average_distance = np.mean(distance_array)
-	return average_distance
-
-
-def remove_nan_col(cord):
-	index = np.isnan(cord).all(axis=1)
-	cord = np.delete(cord, index, axis=0)
-	return cord
-
 
 def rmsd(X, Y):
 	"""
@@ -81,8 +68,12 @@ def pearson(mat1, mat2):
 	r, p = stats.pearsonr(vec1, vec2) # spearmanr
 	return r
 
+def normalize_structure(structure):
+    """Normalize the structure to have unit variance."""
+    max_distance = np.linalg.norm(structure, axis=0).max()
+    return structure / max_distance
 
-def getTransformation(X, Y, centering = False, scaling = True, reflection = False):
+def getTransformation(X, Y, centering = True, scaling = True, reflection = True):
 	"""
 	kabsch method: Recovers transformation needed to align structure1 with structure2.
 	"""
@@ -91,18 +82,16 @@ def getTransformation(X, Y, centering = False, scaling = True, reflection = Fals
 
 	X = X.T
 	Y = Y.T
-	centroid_X = X.mean(axis = 1, keepdims = True)
-	centroid_Y = Y.mean(axis = 1, keepdims = True)
 
-	X = X - centroid_X
-	Y = Y - centroid_Y
-
+	if centering:
+		centroid_X = X.mean(axis = 1, keepdims = True)
+		centroid_Y = Y.mean(axis = 1, keepdims = True)
+		X = X - centroid_X
+		Y = Y - centroid_Y
+	
 	if scaling:
-		scale_X = np.sqrt((X ** 2).sum() / X.shape[1])
-		scale_Y = np.sqrt((Y ** 2).sum() / Y.shape[1])
-		to_scale = scale_Y if type(scaling) is bool else float(scaling)
-		X = X / (scale_X / to_scale)
-		Y = Y / (scale_Y / to_scale)
+		X = normalize_structure(X)
+		Y = normalize_structure(Y)
 
 	C = np.dot(X, Y.transpose())
 	V, _, Wt = np.linalg.svd(C)
@@ -115,14 +104,12 @@ def getTransformation(X, Y, centering = False, scaling = True, reflection = Fals
 	U = np.dot(Wt.T, np.dot(I, V.T))
 	X = np.dot(U, X)
 
-	if not centering:
-		X = X + centroid_Y
-		Y = Y + centroid_Y
 	dx = euclidean_distances(X.T)
 	dy = euclidean_distances(Y.T)
 	pr = pearson(dx, dy)
 	# print(rmsd(X.T, Y.T))
 	return rmsd(X.T, Y.T), X.T, Y.T, pr
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "Compute RMSD and PCC.")
@@ -131,8 +118,8 @@ if __name__ == "__main__":
 	parser.add_argument("--save", help = "Input the second structure.", type=bool, default=False, required = False)
 	args = parser.parse_args()
 
-	structure1, structure2 = np.loadtxt(args.structure1)[51:67], np.loadtxt(args.structure2)[71:87]
-	rmsd, X1, X2, pcc = getTransformation(structure1, structure2)
+	structure1, structure2 = np.loadtxt(args.structure1), np.loadtxt(args.structure2)
+	rmsd, X1, X2, pcc = getTransformation(structure1, structure2) # structure1 is transformed
 	print(f'RMSD: {rmsd}, PCC: {pcc}')
 	if args.save:
 		np.savetxt('structure1.txt', X1)