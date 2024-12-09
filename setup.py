cbda27675a6147698d8d4e2bceec25b5fee6fbd4
kyzhu <kaiyuan-zhu@eng.ucsd.edu>
2024-12-08 16:52:58 -0800

    Tested pip installation.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+	setup(
+	name = 'ec3d',
+	version = '0.0.1',    
+	description = 'Reconstructing the 3D structure of extrachromosomal DNA and other focal amplifications',
+	url = 'https://github.com/AmpliconSuite/ec3D',
+	author = 'Kaiyuan Zhu, Chaohui Li, Biswanath Chowdhary',
+	author_email = 'kaiyuan-zhu@ucsd.edu, chl221@ucsd.edu',
+	license = 'BSD 3-clause',
+	packages = find_packages('src'),
+	install_requires = ['cooler',
+			'iced',
+			'scipy',
+			'scikit-learn',
+			'autograd',
+			'networkx',
+			'python-louvain',
+			'plotly',
+			'kaleido'],
+
+	classifiers = ['Development Status :: 1 - Planning',
+			'Intended Audience :: Science/Research',
+			'License :: OSI Approved :: BSD License',  
+			'Operating System :: POSIX :: Linux',        
+			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.7',],
+	)
